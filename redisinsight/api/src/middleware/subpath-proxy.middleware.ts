21db223ed35bce45d9f4f533b73bb2f5acddd58d
Topher Brown <topher@memfault.com>
2025-03-05 22:34:37 -0500

    fix: update middleware

--- redisinsight/api/src/middleware/subpath-proxy.middleware.ts
+++ redisinsight/api/src/middleware/subpath-proxy.middleware.ts
@@ -8,8 +8,17 @@ export default class SubpathProxyMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
     const originalSendFile = res.sendFile;
     const proxyPath = trim(process.env.RI_PROXY_PATH, '/');
-    res.sendFile = function (this: Response, path: string, options: any, callback?: (err?: Error) => void) {
-      if (path.endsWith('.html') || path.endsWith('.js')) {
+    res.sendFile = function (
+      this: Response,
+      path: string,
+      options: any,
+      callback?: (err?: Error) => void,
+    ) {
+      if (
+        path.endsWith('.html') ||
+        path.endsWith('.js') ||
+        path.endsWith('.css')
+      ) {
         let content = fs.readFileSync(path, 'utf8');
         const regex = /\/?__RIPROXYPATH__/g;
 
